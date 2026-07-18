dd50e8cf64d1d27200a7683a7c451d81ef8b00de
prima6a <prima6a@gmx.at>
2026-07-18 23:46:15 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.FORSTHAENGER_CONFIG = {
+  // Nach der Google-Bereitstellung hier die /exec-URL eintragen:
+  apiUrl: "HIER_GOOGLE_WEBAPP_URL_EINTRAGEN",
+
+  // Persönliche Links funktionieren über ?mitglied=Nachname%20Vorname
+  reservationDays: 60,
+
+  appName: "Forsthänger Gemeinschaft"
+};
---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = "forsthaenger-v5-1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./config.js",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+
+  // Google-API-Anfragen niemals aus dem Cache bedienen.
+  if (url.hostname.includes("script.google.com") || url.hostname.includes("googleusercontent.com")) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
