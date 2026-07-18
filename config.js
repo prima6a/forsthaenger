ba2196977601a43b60e5327963d6346f54a23587
prima6a <prima6a@gmx.at>
2026-07-19 00:06:31 +0200

    Update apiUrl in config.js with Google Web App URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.FORSTHAENGER_CONFIG = {
   // Nach der Google-Bereitstellung hier die /exec-URL eintragen:
-  apiUrl: "HIER_GOOGLE_WEBAPP_URL_EINTRAGEN",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzvj9n99iSgbiSb-fopN2wbs-FYn0CGNvrQOCDd-jdV905apnG4Lzt7NVPkp_zLrf_K/exec",
 
   // Persönliche Links funktionieren über ?mitglied=Nachname%20Vorname
   reservationDays: 60,
